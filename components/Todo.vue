39508ca5c0d5bb4217616b0123bc6a02c2f20beb
snowlong <snowlong2@gmail.com>
2019-08-05 14:48:51 +0900

    first commit

---
+++ components/Todo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <input v-model="message" type="text" />
+    <input v-model="isChecked" type="checkbox">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'hello',
+      isChecked: false,
+    }
+  }
+}
+</script>
+
+<style></style>
